bf761d95e8d259145beb8f107c5b26b2dc758d2b
git <systems@200013.net>
2022-05-05 10:19:26 +0200

    add examples in readme.md

--- target/debug/ipcalc6.d
+++ target/debug/ipcalc6.d
@@ -1,1 +1,1 @@
-/home/luciano/Documents/code/rust/ipcalc6/target/debug/ipcalc6: /home/luciano/Documents/code/rust/ipcalc6/src/main.rs
+/home/luciano/Documents/github/ipcalc6/target/debug/ipcalc6: /home/luciano/Documents/github/ipcalc6/src/main.rs
